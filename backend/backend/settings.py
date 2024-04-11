6709e263cdd667df4ef12dba622433782a1f93ec
Iago Virgílio <iagovirgilio@gmail.com>
2024-04-11 20:13:15 -0300

    Remover configuração banco sqlite3

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -97,10 +97,6 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
-    'postgresql': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
